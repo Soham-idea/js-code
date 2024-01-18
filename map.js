a3182aa9a41a94e1df027f228cde476cf0c2f527
Soham-idea <84754284+Soham-idea@users.noreply.github.com>
2024-01-18 23:04:50 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,15 @@
+
+const array1 = [1,2,3,4,5,6];
+ function (){
+    console.log("hello");
+ }
+//   var a = array1.map(function(arrayele))
+// // for (let val of array1 ) {
+//     console.log(a);
+// }
+  
+  
+// for(let i=0; i<array1.length; i++){
+//   var array2  =array1[i]*2;
+//   console.log(array2);
+// }
